73b95f2472329da6f0f02370a725c025e63d457a
Pranav Kapoorr <pranavkapoorr@gmail.com>
2019-04-03 10:31:06 -0400

    smaller code

--- X/src/com/pranavkapoorr/Solution.java
+++ X/src/com/pranavkapoorr/Solution.java
@@ -30,40 +30,17 @@ package com.pranavkapoorr;
 
 class Solution {
 	protected void DrawX(int size) {
-		for(int i=size==5?size-2:size-1;i>1;i--){ //i>1 to skip last iteration to avoid single *
-			
-			for(int j=1;j<=size-i;j++){//to put  " " right and left of upper section of X
-				System.out.print(" ");
-			}
-			
-			for(int j=1;j<=i*2-1;j++){
-				
-				if(j!=1 && j!=i*2-1) {//to put  " " in upper section of X
-					System.out.print(" ");
-				}else {
-					System.out.print("*");//to put  * in upper section of X
-				}
-				
-			}
-			System.out.println();
-		}
-		
-		for(int i=size==4?2:1;i<=size-(size==5?2:1);i++){//it will include last iteration to put * for X midpoint
-			
-			for(int j=1;j<=size-i;j++){//to put  " " right and left of lower section of X
-				System.out.print(" ");
-			}
-			
-			for(int j=1;j<=i*2-1;j++){
-				
-				if(j!=1 && j!=i*2-1) {//to put  " " lower-middle section of X
-					System.out.print(" ");
-				}else {
-					System.out.print("*");//to put * in lower section of x
-				}
-			}
-			System.out.println();//line change
-		} 
+		for (int i = 1; i <= size; i++) { 
+	            for (int j = 1; j <= size; j++) { 
+	                if ( i == j || j == (size - i + 1)) {            
+	                    System.out.print("*");   
+	                }
+	                else  {         
+	                    System.out.print(" "); 
+	                }
+	            } 
+	            System.out.println(); 
+	        } 
 	}
 	
   public static void main(String[] args) {
