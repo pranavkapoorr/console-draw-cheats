98f8c6803db90a71650e9ca0226504650c8cdaf8
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-04-09 22:48:16 -0400

    numeric-pyramid

--- number-pyramid/src/com/pranavkapoorr/Solution.java
+++ number-pyramid/src/com/pranavkapoorr/Solution.java
@@ -3,14 +3,15 @@ package com.pranavkapoorr;
 /*
 
   
-********1********
-*******2*2*******
-******3*3*3******
-*****4*4*4*4*****
-****5*5*5*5*5****
-***6*6*6*6*6*6***
-**7*7*7*7*7*7*7**
+*******1*******
+******2*2******
+*****3*3*3*****
+****4*4*4*4****
+***5*5*5*5*5***
+**6*6*6*6*6*6**
+*7*7*7*7*7*7*7*
  
+give any size b/w 1-9 , above 9 will deform it a bit
 */
 
 class Solution {
@@ -18,7 +19,7 @@ class Solution {
         
         for (int i = 1; i <= size; i++) { 
           for (int j = 1; j <= size; j++) {
-            if( j<=size-i) {
+            if( j<=size-i+1) {
               System.out.print("*"); 
             }
           }
@@ -27,14 +28,18 @@ class Solution {
               System.out.print("*");
             }
               System.out.print(i);
-              
-      
-		    }
+          }
+          for (int j = 1; j <= size; j++) {
+            if( j<=size-i+1) {
+              System.out.print("*"); 
+            }
+          }
 	        System.out.println(); 
       } 
+      
 	}
 	
   public static void main(String[] args) {
-    new Solution().DrawNumberPyramid(7);
+    new Solution().DrawNumberPyramid(9);
   }
 }
